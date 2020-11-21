02c1772e92145d2e0075d5c7af15d5614137e14b
kittiwat <memoryeamkijkarn@gmail.com>
2020-11-21 14:24:45 +0700

    edit

--- Routers/PagesRouter.jsx
+++ Routers/PagesRouter.jsx
@@ -4,7 +4,6 @@ import { Router, Scene } from 'react-native-router-flux';
 import LoginPage from "../Pages/LoginPage";
 import StoryPage from "../Pages/StoryPage";
 import GamePlayPage from "../Pages/GamePlayPage";
-import Test3D from "../Pages/Test3D";
 
 export default class PagesRouter extends Component {
 
@@ -12,8 +11,7 @@ export default class PagesRouter extends Component {
     return (
       <Router>
         <Scene key="root">
-          <Scene key="test3D" component={Test3D} title="test3D" hideNavBar={true} initial={true} />
-          <Scene key="loginPage" component={LoginPage} title="Login" hideNavBar={true} />
+          <Scene key="loginPage" component={LoginPage} title="Login" hideNavBar={true} initial={true}/>
           <Scene key="storyPage" component={StoryPage} title="Story" hideNavBar={true}/>
           <Scene key="gamePlayPage" component={GamePlayPage} title="MainGame" hideNavBar={true}/>
         </Scene>
